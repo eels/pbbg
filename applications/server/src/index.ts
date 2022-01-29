0f0ad79aaed1d9e55c8ce2b54e4dda4016b89b48
Liam Howell <liam@liam.codes>
2022-01-29 23:09:30 +0000

    feat: seperate docker-compose config into development and production chunks

--- applications/server/src/index.ts
+++ applications/server/src/index.ts
@@ -7,6 +7,6 @@ app.get('/', (_: Request, response: Response) => {
   response.send('Hello world!');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
 });
