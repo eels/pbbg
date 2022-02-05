a44be25bb901c8cb337ccd559566d1e2e01455ff
Liam Howell <liam@liam.codes>
2022-02-05 16:15:25 +0000

    feat: add morgan http logging middleware

--- applications/server/src/providers/middleware.ts
+++ applications/server/src/providers/middleware.ts
@@ -1,8 +1,9 @@
 import app from 'application';
 import { convertEmptyToNull } from 'http/middleware/convert-empty-to-null';
 import { json } from 'body-parser';
+import { morgan } from 'http/middleware/morgan';
 import { trimStrings } from 'http/middleware/trim-strings';
 
-const middleware = [json(), convertEmptyToNull(), trimStrings()];
+const middleware = [json(), convertEmptyToNull(), trimStrings(), morgan()];
 
 middleware.forEach((middleware) => app.use(middleware));
--- applications/server/src/support/facades/log.ts
+++ applications/server/src/support/facades/log.ts
@@ -1,22 +1,35 @@
 import { createLogger, format, transports } from 'winston';
+import type { TransformableInfo } from 'logform';
 
-const logOptions = {
-  format: format.simple(),
+function level(level: string) {
+  return format((info: TransformableInfo) => (info.level === level ? info : false))();
+}
+
+const { simple } = format;
+const { Console, File } = transports;
+
+const transportOptions = [
+  new Console({
+    level: 'info',
+  }),
+  new File({
+    filename: 'logs/combined.log',
+  }),
+  new File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new File({
+    filename: 'logs/http.log',
+    format: level('http'),
+    level: 'http',
+  }),
+];
+
+const log = createLogger({
+  format: simple(),
   level: 'info',
-  transports: [
-    new transports.Console({
-      level: 'info',
-    }),
-    new transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-    }),
-    new transports.File({
-      filename: 'logs/combined.log',
-    }),
-  ],
-};
-
-const log = createLogger(logOptions);
+  transports: transportOptions,
+});
 
 export default log;
