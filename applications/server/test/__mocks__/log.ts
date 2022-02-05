aa5ed979c92d0d294e43819644b6c9fd42e1e938
Liam Howell <liam@liam.codes>
2022-02-05 16:15:25 +0000

    feat: add morgan http logging middleware

--- applications/server/test/__mocks__/log.ts
+++ applications/server/test/__mocks__/log.ts
@@ -2,7 +2,7 @@ interface Logger {
   [key: string]: jest.Mock;
 }
 
-const methods = ['debug', 'error', 'info', 'log', 'warn'];
+const methods = ['debug', 'error', 'http', 'info', 'log', 'warn'];
 const log: Logger = methods.reduce((obj, current) => ({ ...obj, [current]: jest.fn() }), {});
 
 jest.mock('support/facades/log', () => log);
