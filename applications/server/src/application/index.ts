13d4c541829a04bf8a122f07862d8a44bade7e69
Liam Howell <liam@liam.codes>
2022-02-12 11:54:07 +0000

    feat: enable server application trust proxies

--- applications/server/src/application/index.ts
+++ applications/server/src/application/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { PROXY_QUANTITY } from 'config/constants';
 
 const app = express();
+
+app.set('trust proxy', PROXY_QUANTITY);
 
 export default app;
---
+++ applications/server/src/config/constants.ts
@@ -0,0 +1,3 @@
+// --- Rate-limiting ----------------------------
+
+export const PROXY_QUANTITY = 1;
