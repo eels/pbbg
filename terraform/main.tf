55e5967627ea3da92a7b543b855d1380a9edb8f4
Liam Howell <liam@liam.codes>
2022-02-14 17:02:04 +0000

    refactor: standardise terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,48 +38,48 @@ resource "digitalocean_droplet" "server" {
 # --- Firewall ----------------------------------
 
 resource "digitalocean_firewall" "server" {
-  name = "pbbg-http-postgres"
   droplet_ids = [digitalocean_droplet.server.id]
+  name = "pbbg-http-postgres"
   tags = ["docker", "pbbg"]
 
   inbound_rule {
-    protocol = "tcp"
     port_range = "22"
+    protocol = "tcp"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
-    protocol = "tcp"
     port_range = "80"
+    protocol = "tcp"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
-    protocol = "tcp"
     port_range = "443"
+    protocol = "tcp"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
-    protocol = "tcp"
     port_range = "5432"
+    protocol = "tcp"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "1-65535"
     protocol = "tcp"
-    port_range = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    port_range = "1-65535"
     protocol = "udp"
-    port_range = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
-    protocol = "icmp"
     destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "icmp"
   }
 }
