8370710aacd96bb6272918dc215b5b721c5cb58e
Liam Howell <liam@liam.codes>
2022-02-19 11:44:21 +0000

    feat: minify output html from markdown renderer

--- applications/web-client/src/services/MarkdownService.ts
+++ applications/web-client/src/services/MarkdownService.ts
@@ -1,8 +1,14 @@
 import MarkdownIt from 'markdown-it';
 import markdownItAttrs from 'markdown-it-attrs';
+import { minify } from 'html-minifier';
 
 const md = new MarkdownIt();
 
+const minifyOptions = {
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+};
+
 export function convertMarkdownToHTML(markdown: string) {
-  return md.use(markdownItAttrs).render(markdown);
+  return minify(md.use(markdownItAttrs).render(markdown), minifyOptions);
 }
