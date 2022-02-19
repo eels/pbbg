377dcabcd3d0c4b6158f16fc8480314e2e48fea2
Liam Howell <liam@liam.codes>
2022-02-19 17:32:56 +0000

    fix: ensure databse is up before trying to migrate during deployment

--- docker/node/lib/migrate_database.sh
+++ docker/node/lib/migrate_database.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker-compose --file "docker-compose.yml" up --detach postgres
+
 docker-compose --file "docker-compose.production.yml" run \
   --entrypoint "/bin/sh -c 'yarn workspace @pbbg/database-service deploy'" \
   --rm \
