db7102593b88bc48604340268eff45b0a2611895
Liam Howell <liam@liam.codes>
2022-02-20 14:23:59 +0000

    fix: fix reading of secure cookie prefix flag

--- applications/server/src/utilities/cookie.ts
+++ applications/server/src/utilities/cookie.ts
@@ -1,3 +1,3 @@
 export function cookie(cookie: string) {
-  return process.env.APP_USE_SECURE_COOKIE_PREFIX === '1' ? `__Secure-${cookie}` : cookie;
+  return process.env.APP_USE_SECURE_COOKIE_PREFIX ? `__Secure-${cookie}` : cookie;
 }
