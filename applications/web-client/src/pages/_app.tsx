1746be4edf91a6a88cc3f6f60c850b3f488bc98a
Liam Howell <liam@liam.codes>
2022-02-26 23:02:56 +0000

    feat: add web-client manifest and favicons

--- applications/web-client/src/pages/_app.tsx
+++ applications/web-client/src/pages/_app.tsx
@@ -15,9 +15,15 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         <meta name='robots' content='noindex,nofollow,noimageindex' />
+        <meta name='application-name' content='Aethelm' />
+        <meta name='theme-color' content='#111111' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='apple-touch-icon' type='image/png' sizes='180x180' href='apple-icon.png' />
       </Head>
       <Component {...pageProps} />
     </Fragment>
