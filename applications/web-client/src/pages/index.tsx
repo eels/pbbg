76bcf08664f9f312c3487a045ab0e3000b7dcaf0
Liam Howell <liam@liam.codes>
2022-02-27 17:18:00 +0000

    revert: remove dummy example image from web-client homepage

--- applications/web-client/src/pages/index.tsx
+++ applications/web-client/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import Image from 'components/atoms/Image';
-
 export default function Home() {
-  return (
-    <div>
-      <Image layout='fill' src='/images/example.png' />
-    </div>
-  );
+  return <div>Hello world!</div>;
 }
