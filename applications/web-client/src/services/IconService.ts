e69d4f27c727635ebf238f06685fc57b3e5ff326
Liam Howell <liam@liam.codes>
2023-02-03 10:10:06 +0000

    feat: changes to icon service wrapping

--- applications/web-client/src/services/IconService.ts
+++ applications/web-client/src/services/IconService.ts
@@ -25,14 +25,23 @@ export function convertIconToSymbolBySlug(slug: string) {
   const contentCollection = Array.from(svg.children).map((child) => child.outerHTML);
   const contentString = contentCollection.join('');
 
-  svg.removeAttribute('viewBox');
-  svg.setAttribute('style', 'display:none');
-
   symbol.setAttribute('id', slug.replace(/\.svg$/, ''));
   symbol.setAttribute('viewBox', viewBox ?? '');
 
   symbol.innerHTML = contentString;
-  svg.innerHTML = symbol.outerHTML;
+
+  return symbol;
+}
+
+export function wrapIconSymbolsInSVGElement(markup: string) {
+  const dom = new JSDOM();
+  const svg = dom.window.document.createElement('svg');
+
+  svg.setAttribute('fill', 'none');
+  svg.setAttribute('style', 'display:none');
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+  svg.innerHTML = markup;
 
   return svg;
 }
@@ -44,8 +53,8 @@ export function getAllIconSlugs() {
 export function getAllIconsAsSymbols() {
   const slugs = getAllIconSlugs();
   const icons = slugs.map((slug) => convertIconToSymbolBySlug(slug));
-  const filteredIcons = icons.filter((icon): icon is SVGSVGElement => icon !== null);
+  const filteredIcons = icons.filter((icon): icon is HTMLElement => icon !== null);
   const markup = filteredIcons.map((icon) => icon.outerHTML);
 
-  return markup.join('');
+  return wrapIconSymbolsInSVGElement(markup.join('')).outerHTML;
 }
