c7ed1bb878c0ad33c695a82126dde23370f3a4d8
Liam Howell <liam@liam.codes>
2024-08-03 07:33:33 +0100

    refactor: hook up jest from root directory

--- jest.config.cjs
+++ jest.config.cjs
@@ -33,24 +33,29 @@ const defaultConfigOptions = {
 const projects = [
   {
     displayName: '@pbbg/api',
+    moduleDirectories: ['node_modules', 'src'],
     preset: 'ts-jest/presets/js-with-ts-esm',
     roots: ['<rootDir>/applications/api'],
     testEnvironment: 'node',
   },
   {
     displayName: '@pbbg/http',
+    moduleDirectories: ['node_modules', 'src'],
     preset: 'ts-jest',
     roots: ['<rootDir>/packages/http'],
     testEnvironment: 'node',
   },
   {
     displayName: '@pbbg/ui',
+    moduleDirectories: ['node_modules', 'src', 'test'],
+    preset: 'ts-jest/presets/js-with-ts-esm',
     roots: ['<rootDir>/packages/ui'],
     setupFilesAfterEnv: ['<rootDir>/packages/ui/test/setup.ts'],
     testEnvironment: 'jest-environment-jsdom',
   },
   {
     displayName: '@pbbg/utilities',
+    moduleDirectories: ['node_modules', 'src'],
     preset: 'ts-jest',
     roots: ['<rootDir>/packages/utilities'],
     testEnvironment: 'node',
